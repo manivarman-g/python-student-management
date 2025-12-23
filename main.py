2b5299c04e8246ad00c089be38cf9d624386925b
MANIVARMAN G <104122119+manivarman-g@users.noreply.github.com>
2025-12-23 23:31:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+students = []
+def add_students(name):
+   students.append(name)
+def view_students():
+   for students in students :
+     print(students)
+add_student(" rohit")
+add_studnet("virat"_
+view_students()
